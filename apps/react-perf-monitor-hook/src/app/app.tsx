2745f46fbfe6fe3521190acd7db4b56e032e1500
Lonli-Lokli <lonli.lokli@gmail.com>
2024-11-17 22:19:31 +0000

    Initial commit

---
+++ apps/react-perf-monitor-hook/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="react-perf-monitor-hook" />
+    </div>
+  );
+}
+
+export default App;
